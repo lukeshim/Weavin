9d195cd992c28181ea251bb79357d4e3e98d00eb
lukeshim <152858624+lukeshim@users.noreply.github.com>
2024-02-01 22:05:53 +0800

    Add files via upload

---
+++ Weavin/src/main/java/NUS_CodingProject/Weavin/WeavinApplication.java
@@ -0,0 +1,13 @@
+package NUS_CodingProject.Weavin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeavinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeavinApplication.class, args);
+	}
+
+}
---
+++ Weavin/src/test/java/NUS_CodingProject/Weavin/WeavinApplicationTests.java
@@ -0,0 +1,13 @@
+package NUS_CodingProject.Weavin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeavinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
