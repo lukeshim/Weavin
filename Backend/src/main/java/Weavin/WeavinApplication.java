af67f5be4de86063e1de2d79e32f434945704c04
positivelyjon <positivelyjon@gmail.com>
2024-02-08 21:54:16 +0800

    Merge changes

--- Backend/src/main/java/Weavin/WeavinApplication.java
+++ Backend/src/main/java/Weavin/WeavinApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeavinApplication {
+public class
+WeavinApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeavinApplication.class, args);
