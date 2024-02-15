b27b1562b8787f23250154838090869b31badcd7
hjungwoo01 <hjw1129@gmail.com>
2024-02-15 21:57:16 +0800

    Fix Typo

--- Backend/src/main/java/Weavin/WeavinApplication.java
+++ Backend/src/main/java/Weavin/WeavinApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class
-WeavinApplication {
-
+public class WeavinApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WeavinApplication.class, args);
 	}
