97c1b953915e35c664a9839fb4f3ffe284f13eb9
lukeshim <jeffshim6857@gmail.com>
2024-02-19 16:47:37 +0800

    Create a getAllCommentsByMarketPostId, getAllCommentsByForumPostId

--- Backend/src/main/java/Weavin/Controllers/CommentController.java
+++ Backend/src/main/java/Weavin/Controllers/CommentController.java
@@ -2,10 +2,13 @@ package Weavin.Controllers;
 
 import Weavin.Entities.Comment;
 import Weavin.Entities.ForumPost;
+import Weavin.Entities.MarketPost;
 import Weavin.Entities.User;
 import Weavin.Enums.Presence;
 import Weavin.Enums.ReportStatus;
 import Weavin.Repositories.CommentRepository;
+import Weavin.Repositories.ForumPostRepository;
+import Weavin.Repositories.MarketPostRepository;
 import Weavin.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -22,6 +26,10 @@ public class CommentController {
 
     @Autowired
     private CommentRepository commentRepository;
+    @Autowired
+    private MarketPostRepository marketPostRepository;
+    @Autowired
+    private ForumPostRepository forumPostRepository;
 
     @GetMapping("/marketPost/{marketPostId}/comments/{commentid}")
     @ResponseStatus(HttpStatus.OK)
@@ -32,6 +40,27 @@ public class CommentController {
         }
         Comment commentToBeFound = commentOptional.get();
         return commentToBeFound;
+    }
+
+    @GetMapping("/marketPost/{marketPostId}/comments")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Comment> getAllCommentsByMarketPostId(@PathVariable Integer id) {
+        Optional<MarketPost> marketPostOptional = this.marketPostRepository.findById(id);
+        if (marketPostOptional.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Marketpost not found.");
+        }
+        MarketPost marketPostToBeFound = marketPostOptional.get();
+        return marketPostToBeFound.getCommentList();
+    }
+    @GetMapping("/forumPost/{forumPostId}/comments")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Comment> getAllCommentsByForumPostId(@PathVariable Integer id) {
+        Optional<ForumPost> forumPostOptional = this.forumPostRepository.findById(id);
+        if (forumPostOptional.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Forum post not found.");
+        }
+        ForumPost forumPostToBeFound = forumPostOptional.get();
+        return forumPostToBeFound.getCommentList();
     }
 
     @GetMapping("/forumPosts/{forumPostId}/comments/{commentid}")
